eaa00401cc0bf1ba26b39285ec7df364a3ab0c96
sanazkh <sanaz.khosravi70@gmail.com>
2018-08-06 11:29:55 -0700

    zero matrix question added

---
+++ src/com/sjsu/sanaz/ZeroMatrix.java
@@ -0,0 +1,49 @@
+package com.sjsu.sanaz;
+
+/**
+ * Created by sanazk on 8/6/18.
+ */
+public class ZeroMatrix {
+
+    void setZeros(int[][] myMatrix) {
+        boolean[] row = new boolean[myMatrix.length];
+        boolean[] column = new boolean[myMatrix[0].length];
+
+        for (int i = 0; i < myMatrix.length; i++) {
+            for (int j = 0; j < myMatrix[0].length; j++) {
+                if (myMatrix[i][j] == 0) {
+                    row[i] = true;
+                    column[j] = true;
+                }
+            }
+        }
+
+
+        for (int i = 0; i < myMatrix.length; i++) {
+            if (row[i]) {
+                nullifyRow(myMatrix, i);
+            }
+        }
+
+        for (int j = 0; j < myMatrix.length; j++) {
+            if (column[j]) {
+                nullifyColumn(myMatrix, j);
+            }
+        }
+
+
+}
+
+    void nullifyRow(int[][] matrix, int rowIndex) {
+        for(int i = 0; i < matrix[0].length; i++){
+            matrix[rowIndex][i] = 0;
+        }
+    }
+
+    void nullifyColumn(int[][] matrix, int columnIndex) {
+        for(int i = 0; i < matrix.length; i++){
+            matrix[i][columnIndex] = 0;
+        }
+    }
+
+}
